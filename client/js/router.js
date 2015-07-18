5413c254743454a0fb2f6804935326095a476b31
JesseChua94 <jessechua131@gmail.com>
2015-07-18 16:13:33 -0400

    completed structure of the project

---
+++ client/js/router.js
@@ -0,0 +1,10 @@
+Router.configure({
+	layoutTemplate: 'layout' 
+});
+
+Router.map(function() {
+	this.route('Children', {path: '/'});
+	this.route('Parents', {path: '/parents'});
+	this.route('Dashboard', {path: '/dashboard'});
+
+});
