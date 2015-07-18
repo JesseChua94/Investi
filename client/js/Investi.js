c864492704b1b059a5334aea809d28e1c61446a6
064678147 <richardmc.song@mail.utoronto.ca>
2015-07-18 18:08:40 -0400

    Added d3

--- client/js/Investi.js
+++ client/js/Investi.js
@@ -2,13 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.Children.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.Children.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
